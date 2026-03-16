43ebab350f5e0cf76558173d9e3a5f940c580fc7
BEKZATKAZ <bekzatjazz@gmail.com>
2026-03-16 12:24:06 +0500

    perf(low-level): optimize color to hex conversion

--- workspaces/low-level/__tests__/colors/hex.test.ts
+++ workspaces/low-level/__tests__/colors/hex.test.ts
@@ -1,9 +1,11 @@
 import { color, hex } from "colors";
 
 it.each([
-  [255, 255, 255, 255, "#ffffffff"],
+  [255, 255, 255, 255, "#fff"],
   [128, 4, 3, 2, "#80040302"],
-  [0, 0, 0, 0, "#00000000"]
+  [128, 4, 3, 255, "#800403"],
+  [0, 0, 0, 17, "#0001"],
+  [170, 255, 204, 255, "#afc"]
 ])("correctly transforms Color to HEX", (r: number, g: number, b: number, a: number, code: string) => {
   assert.strictEqual(hex(color(r, g, b, a)), hex(code));
 });
--- workspaces/low-level/src/colors/hex.ts
+++ workspaces/low-level/src/colors/hex.ts
@@ -1,5 +1,14 @@
 import { Color, color } from "./color";
 
+const lookUpTable: Record<number, string> = {};
+const lookUpTableSingle: Record<number, string> = {};
+
+for (let i = 0; i < 256; i++) {
+  const result = i.toString(16);
+  lookUpTableSingle[i] = result[0]!;
+  lookUpTable[i] = result.padStart(2, "0");
+}
+
 export type HexColor = `#${string}` & { readonly __brand: unique symbol };
 
 export function hex(color: color): HexColor;
@@ -12,9 +21,33 @@ export function hex(v: color | string): HexColor {
     return v as HexColor;
   }
 
-  const r = Color.r(v).toString(16).padStart(2, "0");
-  const g = Color.g(v).toString(16).padStart(2, "0");
-  const b = Color.b(v).toString(16).padStart(2, "0");
-  const a = Color.a(v).toString(16).padStart(2, "0");
-  return hex(`#${r}${g}${b}${a}`);
+  const r = Color.r(v);
+  const g = Color.g(v);
+  const b = Color.b(v);
+  const a = Color.a(v);
+
+  if (
+    isDoubleHex(Color.r(v)) &&
+    isDoubleHex(Color.g(v)) &&
+    isDoubleHex(Color.b(v)) &&
+    isDoubleHex(Color.a(v))
+  ) {
+    const rh = lookUpTableSingle[r];
+    const gh = lookUpTableSingle[g];
+    const bh = lookUpTableSingle[b];
+    const ah = a === 255 ? "" : lookUpTableSingle[a];
+
+    return `#${rh}${gh}${bh}${ah}` as HexColor;
+  }
+
+  const rh = lookUpTable[r]!;
+  const gh = lookUpTable[g]!;
+  const bh = lookUpTable[b]!;
+  const ah = a === 255 ? "" : lookUpTable[a]!;
+
+  return `#${rh}${gh}${bh}${ah}` as HexColor;
+}
+
+function isDoubleHex(byte: number) {
+  return (byte >> 4) === (byte & 0xF);
 }
